3bfe04b12b2bfd3d49502743805739636844a87a
Fouzan Muhammed K V <fouzanmuhammedkv@gmail.com>
2026-05-24 15:52:29 +0530

    Add files via upload

---
+++ numpalindrome.js
@@ -0,0 +1,29 @@
+
+
+let n = 5;
+
+for (let i = 1; i <= 5; i++)
+{
+    let result= "";
+
+    for (let j = 1; j <= 2 * n-1; j++)
+    {
+        let start = (n - i + 1);
+        let end = (n + i - 1);
+        if (j < start || j > end) 
+        {
+            result += " ";
+        }
+        else if( j <= n )
+        {
+            result += (n - j + 1);
+        }
+        else
+        {
+            result += ( j - n + 1);
+        }
+    }
+    console.log(result);
+   
+   
+}
